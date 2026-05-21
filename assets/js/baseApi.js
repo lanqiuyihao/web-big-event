6103a4b113b143f0a34e6b2d8a73af74eca9bab9
笨小孩 <lanqiuyihao@126.com>
2026-05-21 17:58:59 +0800

    完成首页和头像渲染

--- assets/js/baseApi.js
+++ assets/js/baseApi.js
@@ -1,3 +1,17 @@
 $.ajaxPrefilter(function (options) {
   options.url = 'http://big-event-vue-api-t.itheima.net' + options.url
+
+  if (options.url.indexOf('/my') !== -1) {
+    options.headers = {
+      Authorization: localStorage.getItem('token') || ''
+    }
+  }
+  options.complete = function (res) {
+    if (res.responseJSON.code === 1 && res.responseJSON.message === '身份认证失败！') {
+      // 清空token
+      localStorage.removeItem('token')
+      // 跳转到登录页
+      location.href = '/login.html'
+    }
+  }
 })
---
+++ assets/js/index.js
@@ -0,0 +1,41 @@
+$(function () {
+  getUserInfo()
+
+  let layer = layui.layer
+  $('#logoutBtn').on('click', function () {
+    layer.confirm('您确定要退出吗？', { icon: 3, title: '提示' }, function (index) {
+      //do something
+      // 1.清空本地存储的token
+      localStorage.removeItem('token')
+      // 2.跳转到登录页
+      location.href = '/login.html'
+      layer.close(index)
+    })
+  })
+})
+
+function getUserInfo() {
+  $.ajax({
+    method: 'GET',
+    url: '/my/userinfo',
+    success: function (res) {
+      if (res.code!==0) return layer.msg('获取用户信息失败') 
+      renderAvatar(res.data)
+    }
+  })
+}
+
+function renderAvatar(user) {
+  // 获取用户名称
+  let name = user.username || user.nickname
+  // 设置欢迎文本
+  $('#welcome').html('欢迎，' + name)
+  // 渲染用户头像
+  if (user.user_pic !== null) {
+    $('.layui-nav-img').attr('src', user.user_pic).show()
+    $('.user-avatar').hide()
+  } else {
+    $('.user-avatar').html(name[0].toUpperCase()).show()
+    $('.layui-nav-img').hide()
+  }
+}
